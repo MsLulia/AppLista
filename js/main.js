e7d2209a72fa468fc6f60a39f96c385eee4e3d62
MsLulia <61247450+MsLulia@users.noreply.github.com>
2020-03-21 10:55:30 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ http.createServer((req,res)=>){
 	console.log(`${req.method} solicita ${req.url}`);
 
     if(req.url == '/'){
-        fs.readFile('./App/index.html', 'UTF-8', (err, html) =>{
+        fs.readFile('./index.html', 'UTF-8', (err, html) =>{
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(html);
         });
@@ -16,12 +16,6 @@ http.createServer((req,res)=>){
         const fileStream = fs.createReadStream(reqPath, 'UTF-8');
 
         res.writeHead(200, {'Content-Type': 'text/css'});
-        fileStream.pipe(res);
-    }else if(req.url.match(/.jpg$/)){
-        const reqPath = path.join(__dirname, 'public', req.url);
-        const fileStream = fs.createReadStream(reqPath);
-
-        res.writeHead(200, {'Content-Type': 'image/jpg'});
         fileStream.pipe(res);
     }else{
         res.writeHead(404, {'Content-Type': 'text/plain'});
